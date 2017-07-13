3e684a4385c7d9856bbf699e504b38a71ad6e142
Ray Donnelly <mingw.android@gmail.com>
2017-07-13 05:13:30 -0500

    Add cross-compilation support (WIP)
    Also use conda-build 3's multiple sources feature.
    
    WIP because I either need to remove the (commented out)
    call to autoreconf or I need to comment it back in and
    add autoconf as a build dependency.
    
    The deciding factor here is whether the autoconf changes
    around cross-compilation detection.  We need this to not
    be detected as being cross-compiled as when that happens
    tk assumes strtod is broken (as it assumes it cannot run
    test executables) and builds its own implementation then
    does not link to it (properly?).

--- recipe/build.sh
+++ recipe/build.sh
@@ -7,26 +7,27 @@ if [[ ${ARCH} == 64 ]]; then
     ARCH_FLAG="--enable-64bit"
 fi
 
-cd $SRC_DIR/tcl${VER}/unix
-./configure \
-	--prefix="${PREFIX}" \
-	$ARCH_FLAG \
+pushd tcl${PKG_VERSION}/unix
+  # autoreconf -vfi
+  ./configure  --prefix="${PREFIX}"  \
+               --host=${HOST}        \
+               ${ARCH_FLAG}
+  make
+  make install
+popd
 
-make
-make install
+pushd tk${PKG_VERSION}/unix
+  # autoreconf -vfi
+  ./configure --prefix="${PREFIX}"        \
+              --host=${HOST}              \
+              --with-tcl="${PREFIX}"/lib  \
+              --enable-aqua=yes           \
+              ${ARCH_FLAG}
+  make
+  make install
+popd
 
-cd $SRC_DIR/tk${VER}/unix
-./configure \
-	--prefix="${PREFIX}" \
-	$ARCH_FLAG \
-	--with-tcl="${PREFIX}/lib" \
-	--enable-aqua=yes \
-
-make
-make install
-
-cd $PREFIX
-rm -rf man share
+rm -rf "${PREFIX}"/{man,share}
 
 # Link binaries to non-versioned names to make them easier to find and use.
 ln -s "${PREFIX}"/bin/tclsh${VER_ARR[0]}.${VER_ARR[1]} "${PREFIX}"/bin/tclsh
